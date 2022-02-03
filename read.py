a67ce743ff6bdece55af383e4f0aa71d7550a534
Kmii Yu <idiot3878@gmail.com>
2022-02-03 18:30:46 +0800

    add 篩選系統

--- read.py
+++ read.py
@@ -16,4 +16,11 @@ for d in data:
 
 print('平均留言長度為', sum_len / len(data))
 
-print(data[0])
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new(0))
+
